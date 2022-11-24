1a476ef1788e06aa56dca8b98e52688673991421
caulok <ramicaulo@gmail.com>
2022-11-23 23:34:30 -0300

    first commit

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-let Marker, map;
+let markers = [];
+let map;
 
 function initMap() {
-    const ubicacion1 = { lat: 45, lng: 3 };
-    const ubicacion2 = { lat: 45, lng: 3 };
-    const ubicacion3 = { lat: 45, lng: 3 };
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const posicion = {
+        lat: -25.363,
+        lng: 131.044,
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
-        center: ubicacion,
-    })
+        center: posicion,
+    });
 
-    const Marker = new google.maps.Marker({
-        position: ubicacion,
-        map,
-        title: 'Posición inicial'
-    })
-
-    geoPosiciona()
-}
-
-const geoPosiciona = () => {
-    if (navigator.geolocation) {
-        const geoLoc = navigator.geolocation
-        const options = {timeout: 60000}
-        const watchPos = geoLoc.watchPosition(centraMapa, onError, options)
-        console.log(watchPos)
-    } else {
-        alert("Tu navegador no admite la geolocalización")
-    }
-}
-const centraMapa = (position) =>{
-    const nuevaPos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-    }
-    console.log(nuevaPos)
-    Marker.setPosition(nuevaPos)
-    map.setCenter(nuevaPos)
-}
-
-const onError = (error) => {
-    console.error(error)
+    markers.push(
+        new google.maps.Marker({
+            position: {
+                lat: 43.288773445332524,
+                lng: -2.163845628766325,
+            },
+            map,
+            title: "Zarautz",
+        })
+    );
+    markers.push(
+        new google.maps.Marker({
+            position: {
+                lat: 36.164983935422484,
+                lng: -86.78260332112534,
+            },
+            map,
+            title: "Nashville",
+        })
+    );
+    markers.push(
+        new google.maps.Marker({
+            position: {
+                lat: 36.242150706885596,
+                lng: -6.0775131009957715,
+            },
+            map,
+            title: "El Palmar",
+        })
+    );
 }
